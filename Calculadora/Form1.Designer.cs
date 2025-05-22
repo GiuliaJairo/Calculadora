417121918db51a67e0400fe3041c0d7a58e55b67
Giulia Jairo <jairogiulia93@gmail.com>
2025-05-22 14:59:38 -0300

    Initial commit

---
+++ Calculadora/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace Calculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button0 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.txbTela = new System.Windows.Forms.TextBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.btnMenos = new System.Windows.Forms.Button();
+            this.btnMais = new System.Windows.Forms.Button();
+            this.btnIgual = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button0
+            // 
+            this.button0.BackColor = System.Drawing.Color.Gainsboro;
+            this.button0.Font = new System.Drawing.Font("Futura Md BT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button0.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.button0.Location = new System.Drawing.Point(104, 440);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(165, 70);
+            this.button0.TabIndex = 0;
+            this.button0.Text = "0";
+            this.button0.UseVisualStyleBackColor = false;
+            this.button0.Click += new System.EventHandler(this.numero_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Gainsboro;
+            this.button2.Font = new System.Drawing.Font("Futura Md BT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.button2.Location = new System.Drawing.Point(191, 366);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(79, 70);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.numero_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Gainsboro;
+            this.button3.Font = new System.Drawing.Font("Futura Md BT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.button3.Location = new System.Drawing.Point(273, 366);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(83, 70);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.numero_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gainsboro;
+            this.button1.Font = new System.Drawing.Font("Futura Md BT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.button1.Location = new System.Drawing.Point(104, 366);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 70);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.numero_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Gainsboro;
+            this.button4.Font = new System.Drawing.Font("Futura Md BT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.button4.Location = new System.Drawing.Point(104, 289);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(80, 70);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.numero_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Gainsboro;
+            this.button6.Font = new System.Drawing.Font("Futura Md BT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.button6.Location = new System.Drawing.Point(273, 289);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(83, 70);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.numero_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Gainsboro;
+            this.button5.Font = new System.Drawing.Font("Futura Md BT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.button5.Location = new System.Drawing.Point(191, 289);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(79, 70);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.numero_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Gainsboro;
+            this.button8.Font = new System.Drawing.Font("Futura Md BT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.button8.Location = new System.Drawing.Point(104, 209);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(80, 70);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "7";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.numero_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.Gainsboro;
+            this.button10.Font = new System.Drawing.Font("Futura Md BT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.button10.Location = new System.Drawing.Point(273, 209);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(83, 70);
+            this.button10.TabIndex = 8;
+            this.button10.Text = "9";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.numero_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Gainsboro;
+            this.button9.Font = new System.Drawing.Font("Futura Md BT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.button9.Location = new System.Drawing.Point(191, 209);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(79, 70);
+            this.button9.TabIndex = 7;
+            this.button9.Text = "8";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.numero_Click);
+            // 
+            // txbTela
+            // 
+            this.txbTela.Font = new System.Drawing.Font("HoloLens MDL2 Assets", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbTela.Location = new System.Drawing.Point(82, 75);
+            this.txbTela.Multiline = true;
+            this.txbTela.Name = "txbTela";
+            this.txbTela.ReadOnly = true;
+            this.txbTela.Size = new System.Drawing.Size(358, 64);
+            this.txbTela.TabIndex = 10;
+            this.txbTela.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnLimpar.Font = new System.Drawing.Font("Futura Md BT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.ForeColor = System.Drawing.Color.OrangeRed;
+            this.btnLimpar.Location = new System.Drawing.Point(339, 148);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(101, 55);
+            this.btnLimpar.TabIndex = 11;
+            this.btnLimpar.Text = "⌫";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnDividir.Font = new System.Drawing.Font("Futura Md BT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDividir.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.btnDividir.Location = new System.Drawing.Point(358, 209);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(82, 70);
+            this.btnDividir.TabIndex = 14;
+            this.btnDividir.Text = "/";
+            this.btnDividir.UseVisualStyleBackColor = false;
+            this.btnDividir.Click += new System.EventHandler(this.acoes_Click);
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnMultiplicar.Font = new System.Drawing.Font("Futura Md BT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiplicar.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.btnMultiplicar.Location = new System.Drawing.Point(358, 289);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(82, 70);
+            this.btnMultiplicar.TabIndex = 13;
+            this.btnMultiplicar.Text = "*";
+            this.btnMultiplicar.UseVisualStyleBackColor = false;
+            this.btnMultiplicar.Click += new System.EventHandler(this.acoes_Click);
+            // 
+            // btnMenos
+            // 
+            this.btnMenos.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnMenos.Font = new System.Drawing.Font("Futura Md BT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMenos.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.btnMenos.Location = new System.Drawing.Point(358, 366);
+            this.btnMenos.Name = "btnMenos";
+            this.btnMenos.Size = new System.Drawing.Size(82, 70);
+            this.btnMenos.TabIndex = 12;
+            this.btnMenos.Text = "-";
+            this.btnMenos.UseVisualStyleBackColor = false;
+            this.btnMenos.Click += new System.EventHandler(this.acoes_Click);
+            // 
+            // btnMais
+            // 
+            this.btnMais.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnMais.Font = new System.Drawing.Font("Futura Md BT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMais.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.btnMais.Location = new System.Drawing.Point(358, 440);
+            this.btnMais.Name = "btnMais";
+            this.btnMais.Size = new System.Drawing.Size(82, 70);
+            this.btnMais.TabIndex = 15;
+            this.btnMais.Text = "+";
+            this.btnMais.UseVisualStyleBackColor = false;
+            this.btnMais.Click += new System.EventHandler(this.acoes_Click);
+            // 
+            // btnIgual
+            // 
+            this.btnIgual.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnIgual.Font = new System.Drawing.Font("Futura Md BT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIgual.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.btnIgual.Location = new System.Drawing.Point(273, 440);
+            this.btnIgual.Name = "btnIgual";
+            this.btnIgual.Size = new System.Drawing.Size(83, 70);
+            this.btnIgual.TabIndex = 16;
+            this.btnIgual.Text = "=";
+            this.btnIgual.UseVisualStyleBackColor = false;
+            this.btnIgual.Click += new System.EventHandler(this.btnIgual_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(511, 564);
+            this.Controls.Add(this.btnIgual);
+            this.Controls.Add(this.btnMais);
+            this.Controls.Add(this.btnDividir);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.btnMenos);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.txbTela);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button0);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.TextBox txbTela;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnDividir;
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.Button btnMenos;
+        private System.Windows.Forms.Button btnMais;
+        private System.Windows.Forms.Button btnIgual;
+    }
+}
+
---
+++ Calculadora/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        string expressao = "";
+
+
+        private void numero_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            expressao += btn.Text;
+            txbTela.Text = expressao;
+        }
+
+
+        private void acoes_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            string operador = btn.Text;
+
+            if (string.IsNullOrEmpty(expressao))
+            {
+                return;
+            }
+
+           
+            char ultimoChar = expressao[expressao.Length - 1];
+
+          
+            if ("+-*/".Contains(ultimoChar))
+            {
+                expressao = expressao.Remove(expressao.Length - 1);
+            }
+
+            expressao += operador;
+            txbTela.Text = expressao;
+
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            expressao = expressao.Substring(0, expressao.Length - 1);
+            txbTela.Text = expressao;
+            
+                
+        }
+
+        private void btnIgual_Click(object sender, EventArgs e)
+        {
+
+            char ultimoChar = expressao[expressao.Length - 1];
+            if ("+-*/".Contains(ultimoChar))
+            {
+                MessageBox.Show("Termina de digitar!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            
+            
+           
+
+
+            if (expressao.Contains("/0"))
+            {
+                MessageBox.Show("Dividir por zero pra quê?", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                expressao = "";
+                txbTela.Text = "";
+            }
+           
+            else
+            {
+                var resultado = new DataTable().Compute(expressao, null);
+                txbTela.Text = resultado.ToString();
+                expressao = resultado.ToString();
+            }
+            
+        }
+    }
+}
